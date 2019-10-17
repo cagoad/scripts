de2758ad3579de4ebcb32bf5e7792647c4710045
Cameron Goad <cameron.goad@liveramp.com>
2019-10-17 15:30:03 -0500

    add simple vendor alias script

---
+++ createBucketAliases.py
@@ -0,0 +1,9 @@
+filename = "/Users/cagoad/dev/bucketMapping.csv"
+dict = {}
+with open(filename) as f:
+    for line in f:
+        (key, val) = line.rstrip("\n").split(",")
+        dict[key] = val
+        
+for key, value in dict.iteritems():
+    print("alias " + key +"='gull {}'").format(value)
